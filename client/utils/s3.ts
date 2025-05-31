0ac6906b2d6ad35caebdc3617b691d4b1cba64bf
itsanmol27 <anmolgarg271005@gmail.com>
2025-05-31 21:57:16 +0530

    upgrade aws sdk

--- client/utils/s3.ts
+++ client/utils/s3.ts
@@ -1,11 +1,12 @@
-import AWS from "aws-sdk"
+import { S3Client } from "@aws-sdk/client-s3";
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
-    region: process.env.AWS_S3_REGION,
-    s3ForcePathStyle: true,
-    signatureVersion: 'v4'
-})
+const s3 = new S3Client({
+  region: process.env.AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY!,
+  },
+  forcePathStyle: true,
+});
 
 export default s3;
