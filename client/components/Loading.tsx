2e93cbe80a0451bf7ba537b7ba24e93720c78155
itsanmol27 <anmolgarg271005@gmail.com>
2025-06-03 13:42:18 +0530

    "improve loader"

--- client/components/Loading.tsx
+++ client/components/Loading.tsx
@@ -1,10 +1,10 @@
-import { Loader } from 'lucide-react'
+import { LoaderCircle } from 'lucide-react'
 import React from 'react'
 
 const Loading = () => {
     return (
         <div className=' w-full h-full flex items-center justify-center'>
-            <Loader />
+            <LoaderCircle className=" w-10 h-10 animate-spin" />
         </div>
     )
 }
